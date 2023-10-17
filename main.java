fc6fbba8ff03647d7a412a418a0446f725e32958
Yago Santos <yagprogramr@gmail.com>
2023-10-17 19:13:00 -0300

    initial code

---
+++ main.java
@@ -0,0 +1,123 @@
+class Minecraft {
+  float versao;
+
+  void mecanicasJogo() {
+      // Implementação
+  }
+}
+
+class ModMinecraft extends Minecraft {
+  String nome;
+
+  void modificarJogo() {
+      // Implementação
+  }
+}
+
+class MinecraftForge {
+  float versao;
+
+  void liberaCodigo() {
+      // Implementação
+  }
+}
+
+class NewGameMode {
+  int gameMode;
+  String gameModeName;
+
+  void facilitaConstrucao() {
+      // Implementação
+  }
+}
+
+class RemovePlaceBlockLimit {
+  int gameMode;
+
+  void removeLimiteDistancia() {
+      // Implementação
+  }
+}
+
+class NewGameModeMenu {
+  int nameMode;
+  String gameModeName;
+
+  void mostraInterface() {
+      // Implementação
+  }
+}
+
+class ActivationMode {
+  String modo; // (on, off)
+
+  void liga() {
+      // Implementação
+  }
+
+  void desliga() {
+      // Implementação
+  }
+}
+
+class PaintingTool {
+  int tamanho;
+  String materialLista;
+  int numeroBloco;
+
+  void recolocarBloco() {
+      // Implementação
+  }
+}
+
+class SimetricPlaceBlock {
+  String tipoDeBloco;
+  int eixoDeSimetria;
+
+  void colocarBloco(int x, int y, int z) {
+      // Implementação
+  }
+}
+
+class InventoryBuild {
+  float memoriaUsada;
+  String armazenaEstruturas;
+
+  void colocaEstruturas() {
+      // Implementação
+  }
+}
+
+class PlacingDelay {
+  int tempoDelay;
+
+  void mudaIntervaloColocacao() {
+      // Implementação
+  }
+}
+
+class MultiPlacing {
+  int definirDensidade;
+
+  void colocarBlocos() {
+      // Implementação
+  }
+}
+
+class LimitingGrid {
+  void ativacaoX() {
+      // Implementação
+  }
+
+  void ativacaoY() {
+      // Implementação
+  }
+
+  void ativacaoZ() {
+      // Implementação
+  }
+
+  void limitaBlocos() {
+      // Implementação
+  }
+}
